fb36d3644114b28bb1f8459ea442413e273f13fe
physicsgtx <60617282+physicsgtx@users.noreply.github.com>
2021-10-02 15:14:08 +0530

    updated as1q1.c
    reduced variables and space complexity.

--- as1q1.c
+++ as1q1.c
@@ -1,12 +1,11 @@
 /*By: Yash Mudgal
-Date:07/09/2019*/
+Date:07/09/2021 */
 #include<stdio.h>
 void main()
 {
-int a,b,area;
+int a,b;
 printf("enter length and breadth ");
 scanf("%d%d",&a,&b);
-area=a*b;
-printf("area = %d",area);
+printf("area = %d",a*b); /*simply return in printf without using another variable*/
 return 0;
 }
